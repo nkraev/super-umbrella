67cb9865f8d0ff28c6a1c3d8d22cdce3f2457867
Nikita Kraev <kitttn9@gmail.com>
2025-02-23 12:38:55 -0100

    add: Adding Google Maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
 
     implementation (libs.retrofit)
     implementation (libs.converter.moshi)
+    implementation(libs.maps.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
+import com.google.maps.android.compose.GoogleMap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            GoogleMap()
         }
     }
 }
